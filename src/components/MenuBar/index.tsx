2893e684879d12fb06c7fb4b69cf781e2a4cb395
lucassiqz <lucassiqzro@gmail.com>
2020-07-19 23:45:24 -0300

    Create MenuBar

---
+++ src/components/MenuBar/index.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+
+import Button from '../Button';
+
+import {
+  Container,
+  Topside,
+  Logo,
+  MenuButton,
+  HomeIcon,
+  BellIcon,
+  FavoriteIcon,
+  ProfileIcon,
+  EmailIcon,
+  Botside,
+  Avatar,
+  ProfileData,
+  ExitIcon,
+} from './styles';
+
+const MenuBar: React.FC = () => {
+  return (
+    <Container>
+      <Topside>
+        <Logo />
+
+        <MenuButton>
+          <HomeIcon />
+          <span>Pagina Inicial</span>
+        </MenuButton>
+
+        <MenuButton>
+          <BellIcon />
+          <span>Notificações</span>
+        </MenuButton>
+
+        <MenuButton>
+          <EmailIcon />
+          <span>Mensagens</span>
+        </MenuButton>
+
+        <MenuButton>
+          <FavoriteIcon />
+          <span>Favoritados</span>
+        </MenuButton>
+
+        <MenuButton className="active">
+          <ProfileIcon />
+          <span>Perfil</span>
+        </MenuButton>
+
+        <Button>
+          <span>Tweetar</span>
+        </Button>
+      </Topside>
+
+      <Botside>
+        <Avatar />
+
+        <ProfileData>
+          <strong>Lucas Siqueira</strong>
+          <strong>@lucassiqz</strong>
+        </ProfileData>
+
+        <ExitIcon />
+      </Botside>
+    </Container>
+  );
+};
+
+export default MenuBar;
